db54c61ef06cf61b1152cb031a02616e846164b7
Nicolas Lopez <niclopezt@gmail.com>
2024-04-21 22:10:06 +1000

    Creates linked list methods: prepend (add node at the start) size (list lenght) at (to get node at specified index) pop (deletes last node) contains (gets node with specific value)

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -63,20 +63,61 @@ const SLinkedList = class {
         return printedList;
     }
 
-    // get size() {
-    //     return this.#sLinkedList.length;
-    // }
+    size() {
+        /**
+         * Get the number of nodes
+         */
 
-    get head() {
+        let tmpNode = this.#firstNode;
+        let totalNodes = 0;
+
+        while (tmpNode instanceof Node) {
+            totalNodes++;
+            tmpNode = tmpNode.nextNode;
+        }
+
+        return totalNodes;
+    }
+
+    at(index) {
+        /**
+         * returns the node at the given index
+         */
+
+        let tmpNode = this.#firstNode;
+        let totalNodes = 0;
+
+        while (tmpNode instanceof Node) {
+            if (totalNodes === index) return tmpNode;
+            totalNodes++;
+            tmpNode = tmpNode.nextNode;
+        }
+
+        return console.error(`Index (${index}) out of range`);
+    }
+
+    head() {
+        /**
+         * Get the first node
+         */
+
         return this.#firstNode;
     }
 
-    set head(newFirstNode) {
+    prepend(newFirstNode) {
+        /**
+         * Append a new Node to the start of the list
+         */
+
         newFirstNode.nextNode = this.#firstNode;
         this.#firstNode = newFirstNode;
     }
 
-    get tail() {
+    tail() {
+        /**
+         * Get the last node
+         */
+
         return this.#checkAllNodes(this.#firstNode);
     }
 
@@ -89,7 +130,35 @@ const SLinkedList = class {
 
         this.#firstNode === null
             ? (this.#firstNode = newNode)
-            : (this.tail.nextNode = newNode);
+            : (this.tail().nextNode = newNode);
+    }
+
+    pop() {
+        /**
+         * removes the last element from the list
+         */
+
+        this.at(this.size() - 2).nextNode = null;
+    }
+
+    contains(value) {
+        /**
+         *  returns true if the passed in value is in the list and otherwise returns false.
+         */
+        let tmpNode = this.#firstNode;
+
+        while (tmpNode instanceof Node) {
+            if (`${tmpNode.value}` === value) return true;
+            tmpNode = tmpNode.nextNode;
+        }
+
+        return false;
+    }
+
+    find() {
+        /**
+         * returns the index of the node containing value, or null if not found.
+         */
     }
 };
 
@@ -111,12 +180,29 @@ sLL.append(nodeAux);
 // console.log(`${sLL}`);
 
 // Get first node
-// console.log(`${sLL.head}`);
+// console.log(`${sLL.head()}`);
 
 // Set first node
-// sLL.head = nodeTest;
-// console.log(`${sLL.head}`);
+// sLL.prepend(nodeTest);
+// console.log(`${sLL.head()}`);
 // console.log(`${sLL}`);
 
 // Get last node
-// console.log(`${sLL.tail}`);
+// console.log(`${sLL.tail()}`);
+
+// # of nodes
+// console.log(sLL.size());
+
+// Get node at the given index
+// console.log(`${sLL.at(0)}`);
+// console.log(`${sLL.at(1)}`);
+// console.log(`${sLL.at(4)}`);
+
+// removes the last element from the list
+// console.log(`${sLL}`);
+// sLL.pop();
+// console.log(`${sLL}`);
+
+//  returns true if the passed in value is in the list and otherwise returns false.
+// console.log(sLL.contains("nodeAux"));
+// console.log(sLL.contains("nodeTest"));
